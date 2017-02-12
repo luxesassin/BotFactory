2ce4bcbff93ec26fe1439ea8fdf0ef4d166e1ee8
luxes <luxeser@gmail.com>
2017-02-12 13:00:24 -0800

    close #1 - Added the history page

---
+++ application/views/History.php
@@ -0,0 +1,30 @@
+<h1>Transaction History</h1>
+
+<div class="row">
+    <table class="table">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Transaction ID</th>
+                <th>Transaction Date</th>
+                <th>Type</th>
+                <th>Amount</th>
+                <th>Details</th>
+                <th>Valid</th>
+            </tr>
+        </thead>
+        <tbody>
+            {histories}
+            <tr>
+                <td>{id}</td>
+                <td>{transId}</td>
+                <td>{transDate}</td>
+                <td>{type}</td>
+                <td>{amount}</td>
+                <td>{detail}</td>
+                <td>{isValid}</td>
+            </tr>
+            {/histories}
+        </tbody>
+    </table>
+</div>

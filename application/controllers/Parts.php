ca301d68fea946d6ad075ed07d5ed3eecffc0730
Tim Makimov <megapers@gmail.com>
2017-03-30 20:13:48 -0700

    Added tooltip to each part

--- application/controllers/Parts.php
+++ application/controllers/Parts.php
@@ -7,8 +7,15 @@
 class Parts extends Application{
     public function index(){
         $this->data['pagebody'] = 'parts';
-        $this->data['parts'] = $this->factory->all('Parts');
-        $this->render();
+		$source = $this->factory->all('parts'); // get all data in Parts table in the db.
+		$partArr = array(); // empty array.
+		foreach ($source as $record) {
+			$partArr[] = array ('image' => $record->image, 'partname' => $record->ca, 'line' => $record->builtAt );
+		} 
+        $this->data['parts'] = $partArr;
+        $this->data['title'] = 'test';
+		$this->render();
+		
     }
 }
 

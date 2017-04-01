ad3c2868d14134f2ea85225b0a8d784a9224b8c8
Yiaoping <yiao2873@hotmail.com>
2017-03-31 17:26:32 -0700

    closes job6
    Signed-off-by: Yiaoping <yiao2873@hotmail.com>

--- application/controllers/Manage.php
+++ application/controllers/Manage.php
@@ -14,7 +14,45 @@ class Manage extends Application
         }
         
         $this->data['pagebody'] = 'manage';
+        $this->data['registered'] = isset($this->session->apikey);
         $this->render();
+    }
+    
+    function rebootme(){
+        if(isset($this->session->apikey)){
+            $this->load->library("unirest");
+            $url = 'https://umbrella.jlparry.com/work/rebootme?key=' . $this->session->apikey;       
+            $response = $this->unirest->get($url, $headers = array());
+
+            if($response->body == "Ok"){
+                //TODO delete history and inventory in db
+                
+            }else{
+
+            }
+        }else{
+            //showing error message 
+        }
+        redirect('manage');
+    }
+    
+    function register(){
+      if (isset($_POST['plant']) && isset($_POST['token'])) {
+        $this->load->library("unirest");
+        $url = 'https://umbrella.jlparry.com/work/registerme/'. $_POST['plant'] . '/' . $_POST['token'];       
+        $response = $this->unirest->get($url, $headers = array());
+
+           $data = explode(" ", $response->body);
+            if($data[0] == "Ok"){
+                $this->session->apikey = $data[1]; 
+            }else{
+                //show $response->body
+            }
+      }else {
+          //showing error for requied inputs
+      }
+      redirect('manage');
+
     }
 }
 

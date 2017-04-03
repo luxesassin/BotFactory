38b57c8e0d38c78716cb452ad1cb3b8a1b0ec8af
luxes <luxeser@gmail.com>
2017-04-02 17:31:13 -0700

    Closing #5

--- application/controllers/History.php
+++ application/controllers/History.php
@@ -7,6 +7,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  */
 class History extends Application
 {
+    private $items_per_page = 20;
+    private $pageNum = 1;
+
     function __construct() 
     {
         parent::__construct();
@@ -14,12 +17,139 @@ class History extends Application
     
     public function index()
     {
+        $this->page(1);
+    }
+
+    // Extract & handle a page of items, defaulting to the beginning
+    function page($num = 1)
+    {
+        $this->pageNum = $num;
+
+        $records = $this->factory->all('History'); // get all the history
+        $arrHistory = array(); // start with an empty extract
+        // use a foreach loop, because the record indices may not be sequential
+        $index = 0; // where are we in the tasks list
+        $count = 0; // how many items have we added to the extract
+        $start = ($num - 1) * $this->items_per_page;
+        foreach($records as $history)
+        {
+            if ($index++ >= $start)
+            {
+                $arrHistory[] = $history;
+                $count++;
+            }
+            if ($count >= $this->items_per_page)
+                break;
+        }
+        $this->data['pagination'] = $this->pagenav($num);
+        $this->show_page($arrHistory);
+    }
+
+    // Show a single page of history items
+    private function show_page($source)
+    {
         $this->data['pagebody'] = 'history';
 
-        // build the list of authors, to pass on to our view
-        $source = $this->factory->all('History');
+        // build the list of history, to pass on to our view
         $this->data['histories'] = $source;
 
         $this->render();
     }
+
+    // Build the pagination navbar
+    private function pagenav($num)
+    {
+        $records = $this->factory->all('History');
+        $totalHistory = 0;
+        foreach($records as $history)
+            $totalHistory++;
+        $lastpage = ceil($totalHistory / $this->items_per_page);
+        $parms = array(
+            'first' => 1,
+            'previous' => (max($num-1,1)),
+            'next' => min($num+1,$lastpage),
+            'last' => $lastpage
+        );
+
+        return $this->parser->parse('itemnav',$parms,true);
+    }
+
+    // Set the number of records per page.
+    public function recordsPerPage($num = 20)
+    {
+        $this->items_per_page = $num;
+        //redirect($_SERVER['HTTP_REFERER']); // back where we came from
+
+        $this->page($this->pageNum);
+    }
+
+    public function orderByDateTime($num = 1)
+    {
+        $this->pageNum = $num;
+
+        $records = $this->factory->all('History'); // get all the history
+        usort($records, "cmpByDateTime");
+        $arrHistory = array(); // start with an empty extract
+        // use a foreach loop, because the record indices may not be sequential
+        $index = 0; // where are we in the tasks list
+        $count = 0; // how many items have we added to the extract
+        $start = ($num - 1) * $this->items_per_page;
+        foreach($records as $history)
+        {
+            if ($index++ >= $start)
+            {
+                $arrHistory[] = $history;
+                $count++;
+            }
+            if ($count >= $this->items_per_page)
+                break;
+        }
+        $this->data['pagination'] = $this->pagenav($num);
+        $this->show_page($arrHistory);
+    }
+
+    public function orderByrobotmodel($num = 1)
+    {
+    $this->pageNum = $num;
+
+        $records = $this->factory->all('History'); // get all the history
+        usort($records, "cmpByRobotModel");
+        $arrHistory = array(); // start with an empty extract
+        // use a foreach loop, because the record indices may not be sequential
+        $index = 0; // where are we in the tasks list
+        $count = 0; // how many items have we added to the extract
+        $start = ($num - 1) * $this->items_per_page;
+        foreach($records as $history)
+        {
+            if ($index++ >= $start)
+            {
+                $arrHistory[] = $history;
+                $count++;
+            }
+            if ($count >= $this->items_per_page)
+                break;
+        }
+        $this->data['pagination'] = $this->pagenav($num);
+        $this->show_page($arrHistory);   
+    }
+}
+
+function cmpByDateTime($a, $b)
+{
+    if ($a->transDate < $b->transDate)
+        return -1;
+    elseif ($a->transDate > $b->transDate)
+        return 1;
+    else
+        return 0;
+}
+
+function cmpByRobotModel($a, $b)
+{
+    if ($a->transId < $b->transId)
+        return -1;
+    elseif ($a->transId > $b->transId)
+        return 1;
+    else
+        return 0;
 }
--- application/views/History.php
+++ application/views/History.php
@@ -1,5 +1,18 @@
 <h1>Transaction History</h1>
 
+<div class="dropdown pull-right">
+    <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Records/Page
+    <span class="caret"></span></button>
+    <ul class="dropdown-menu">
+        <li><a href="/History/recordsPerPage/20">20</a></li>
+        <li><a href="/History/recordsPerPage/25">25</a></li>
+    </ul>
+</div>
+
+<a href="/history/orderByDateTime/1"><input type="button" value="Order by datetime"/></a>
+<a href="/history/orderByrobotmodel/1"><input type="button" value="Order by robot model"/></a>
+
+{pagination}
 <div class="row">
     <table class="table">
         <thead>
@@ -28,4 +41,5 @@
         </tbody>
     </table>
 </div>
+
 <p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
